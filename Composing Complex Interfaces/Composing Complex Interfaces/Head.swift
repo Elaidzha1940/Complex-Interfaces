dc8f49934ca2a1c9f53982ef58933d8276301ac4
Elaidzha1940 <eli080293@gmail.com>
2023-05-24 21:37:52 +0700

    added more Files

---
+++ Composing Complex Interfaces/Composing Complex Interfaces/Head.swift
@@ -0,0 +1,20 @@
+//
+//  Head.swift
+//  Composing Complex Interfaces
+//
+//  Created by Elaidzha Shchukin on 24.05.2023.
+//
+
+import SwiftUI
+
+struct Head: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Head_Previews: PreviewProvider {
+    static var previews: some View {
+        Head()
+    }
+}
