42e360a92fb6a30606e97ef637a20191583fa123
Elaidzha1940 <eli080293@gmail.com>
2023-05-24 20:27:51 +0700

    Composing Complex Interfaces

---
+++ Composing Complex Interfaces/Composing Complex Interfaces/ContentView.swift
@@ -0,0 +1,26 @@
+//  /*
+//
+//  Project: Composing Complex Interfaces
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.05.2023
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Text("Hello, world!")
+                .foregroundColor(.purple)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
