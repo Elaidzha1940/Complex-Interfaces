c887341079e3a2d03d1e99f1bcdfccbf542997c9
Elaidzha1940 <eli080293@gmail.com>
2023-05-24 20:37:44 +0700

    added a new Folder / File + ModelData

--- Composing Complex Interfaces/Composing Complex Interfaces/CategoryHome.swift
+++ Composing Complex Interfaces/Composing Complex Interfaces/CategoryHome.swift
@@ -10,10 +10,16 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+
     var body: some View {
         NavigationView {
-            Text("Hello, World!")
-                .navigationTitle("Featured")
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
         }
     }
 }
@@ -21,5 +27,6 @@ struct CategoryHome: View {
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
+            .environmentObject(ModelData())
     }
 }
--- Composing Complex Interfaces/Composing Complex Interfaces/Model/ModelData.swift
+++ Composing Complex Interfaces/Composing Complex Interfaces/Model/ModelData.swift
@@ -10,14 +10,36 @@
 import Foundation
 import Combine
 
-struct ModelData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
     }
 }
 
-struct ModelData_Previews: PreviewProvider {
-    static var previews: some View {
-        ModelData()
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
