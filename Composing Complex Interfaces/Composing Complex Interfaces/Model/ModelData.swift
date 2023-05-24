8ab4d8b1cf6d4bf25bcfbc14311ffda2732076d1
Elaidzha1940 <eli080293@gmail.com>
2023-05-24 20:34:27 +0700

    added a new Folder / File + ModelData

---
+++ Composing Complex Interfaces/Composing Complex Interfaces/Model/ModelData.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Composing Complex Interfaces
+//  File: ModelData.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.05.2023
+//
+//  */
+
+import Foundation
+import Combine
+
+struct ModelData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModelData_Previews: PreviewProvider {
+    static var previews: some View {
+        ModelData()
+    }
+}
