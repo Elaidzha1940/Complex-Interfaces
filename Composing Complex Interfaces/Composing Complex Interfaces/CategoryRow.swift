3aff6825ed29c248c73f9f439f5a1d358a81d3b7
Elaidzha1940 <eli080293@gmail.com>
2023-05-24 21:44:46 +0700

    added more Files

--- Composing Complex Interfaces/Composing Complex Interfaces/CategoryRow.swift
+++ Composing Complex Interfaces/Composing Complex Interfaces/CategoryRow.swift
@@ -12,14 +12,14 @@ import SwiftUI
 struct CategoryRow: View {
     var categoryName: String
     var items: [Landmark]
-    
+
     var body: some View {
         VStack(alignment: .leading) {
             Text(categoryName)
                 .font(.headline)
                 .padding(.leading, 15)
                 .padding(.top, 5)
-            
+
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
@@ -27,7 +27,8 @@ struct CategoryRow: View {
                             LandmarkDetail(landmark: landmark)
                         } label: {
                             CategoryItem(landmark: landmark)
-                        }                    }
+                        }
+                    }
                 }
             }
             .frame(height: 185)
@@ -37,7 +38,7 @@ struct CategoryRow: View {
 
 struct CategoryRow_Previews: PreviewProvider {
     static var landmarks = ModelData().landmarks
-    
+
     static var previews: some View {
         CategoryRow(
             categoryName: landmarks[0].category.rawValue,
--- Composing Complex Interfaces/Composing Complex Interfaces/Head.swift
+++ Composing Complex Interfaces/Composing Complex Interfaces/Head.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct Head: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @State private var selection: Tab = .featured
+
+     enum Tab {
+         case featured
+         case list
+     }
+
+     var body: some View {
+         TabView(selection: $selection) {
+             CategoryHome()
+                 .tabItem {
+                     Label("Featured", systemImage: "star")
+                 }
+                 .tag(Tab.featured)
+
+             LandmarkList()
+                 .tabItem {
+                     Label("List", systemImage: "list.bullet")
+                 }
+                 .tag(Tab.list)
+         }
+     }
+ }
 
 struct Head_Previews: PreviewProvider {
     static var previews: some View {
         Head()
+            .environmentObject(ModelData())
     }
 }
